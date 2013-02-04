871ab5eb29be39977d6651e68786a9babe5a0634
PhoenixFlight <ren1us@zephyrr.com>
2013-02-03 21:10:28 -0500

    Basic world displays

--- Aero/src/com/zephyrr/aero/gui/Window.java
+++ Aero/src/com/zephyrr/aero/gui/Window.java
@@ -5,18 +5,20 @@ import java.util.HashMap;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
 
+import com.zephyrr.aero.entry.Main;
 import com.zephyrr.aero.world.ChunkGrid;
 
 public class Window extends JFrame {
-	JLayeredPane deepWorldPane;
-	private HashMap<Integer, ChunkGrid> gridMap;
-	public Window(int x, int y) {
+	private WorldPane deepWorldPane;
+	private Main main;
+	public Window(Main m, int x, int y) {
+		main = m;
 		setSize(800,600);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		deepWorldPane = new JLayeredPane();
-		gridMap = new HashMap<Integer, ChunkGrid>();
+		deepWorldPane = new WorldPane(this);
+		add(deepWorldPane);
 	}
 	public void update() {
 		
--- Aero/src/com/zephyrr/aero/world/World.java
+++ Aero/src/com/zephyrr/aero/world/World.java
@@ -1,5 +1,37 @@
 package com.zephyrr.aero.world;
 
-public abstract class World {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public abstract class World {
+	private static ArrayList<World> worlds;
+	private static World activeWorld;
+	static {
+		worlds = new ArrayList<World>();
+		loadWorld("mainworld.wld");
+	}
+	public static World getActiveWorld() {
+		return activeWorld;
+	}
+	public static void setActiveWorld(World w) {
+		activeWorld = w;
+	}
+	private static World loadWorld(String path) {
+		// PLACEHOLDER
+		return null;
+	}
+	public static ArrayList<World> getWorlds() {
+		return worlds;
+	}
+	
+	private HashMap<Integer, ChunkGrid> layers;
+	protected World() {
+		layers = new HashMap<Integer, ChunkGrid>();
+	}
+	public HashMap<Integer, ChunkGrid> getLayers() {
+		return layers;
+	}
+	public ChunkGrid getLayer(int level) {
+		return layers.get(level);
+	}
 }
