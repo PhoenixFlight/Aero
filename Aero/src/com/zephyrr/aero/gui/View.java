8568b57e8e6fdbbdfaab0bcda270db4871e0a030
PhoenixFlight <ren1us@zephyrr.com>
2013-02-03 20:42:41 -0500

    Added image cache

--- Aero/src/com/zephyrr/aero/gui/View.java
+++
@@ -1,7 +0,0 @@
-package com.zephyrr.aero.gui;
-
-import javax.swing.JPanel;
-
-public abstract class View extends JPanel {
-	
-}
--- Aero/src/com/zephyrr/aero/gui/WorldView.java
+++
@@ -1,8 +0,0 @@
-package com.zephyrr.aero.gui;
-
-import com.zephyrr.aero.world.World;
-
-public class WorldView extends View {
-	private World toView;
-	
-}
